dd6a06b775ec68425110ff93dfa8ca65f293f106
egp <egp@egp.net>
2017-07-22 13:48:18 -0700

    misc changes

--- src/main/scala/net/egp/puzzle/Themes.scala
+++ src/main/scala/net/egp/puzzle/Themes.scala
@@ -1,13 +1,16 @@
 package net.egp.puzzle
 
+import better.files.{File => BFile}
+import buildinfo.BuildInfo
 import net.egp.puzzle.Util._
 import org.scalactic.TypeCheckedTripleEquals._
-import better.files.{File => BFile}
 
 object Themes extends App {
   assert(args.length === 1, "Expecting one argument: Theme name (without extension) found in src/main/resources/")
   val themeName = args(0)
-  println(s"Starting Themes: theme-file=$themeName.names, phrase-file=$themeName.phrases")
+  val myName = BuildInfo.name
+  val buildInfoString = s"$myName ${BuildInfo.version} sbt ${BuildInfo.sbtVersion} scala ${BuildInfo.scalaVersion}"
+  println(s"$buildInfoString\n theme-file=$themeName.names, phrase-file=$themeName.phrases")
   val attempts = new Themes(themeName).solve()
   val possibleSolutions = attempts.filter(_.isComplete) // keep ones that have 13 room names
   val solutions = possibleSolutions.filter(_.isValid) // keep ones without contention for names
@@ -21,7 +24,7 @@ object Themes extends App {
       "No solutions found.\n"
     }
   }
-  println(s"Complete: Themes tried ${attempts.length} cases,  " +
+  println(s"Complete: $myName tried ${attempts.length} cases,  " +
     s"Wrote ${solutions.length} solutions to ${solutionFile.path}")
 }
 
@@ -29,22 +32,21 @@ class Themes(theme: String) {
   lazy val requireNumberOfNames = 13
   val phraseList: Seq[Seq[Char]] = readFromResource(s"$theme.phrases").map(_.toSeq).filterNot(_.contains('0'))
   phraseList.foreach(p => assert(requireNumberOfNames === p.length))
-  val exclusionlist: Seq[String] = readFromResource("exclusions.txt")
-  val dictLines: Seq[String] = readFromResource(s"$theme.names")
-  val rawDictList: Seq[String] = dictLines.filterNot(exclusionlist.contains)
-  println(s"read ${phraseList.length} phrases, ${dictLines.length} theme words and ${exclusionlist.length} exclusions")
-  val wordsByLength: Map[Int, Seq[String]] = rawDictList.groupBy(_.length)
+  val nameLines: Seq[String] = readFromResource(s"$theme.names")
+  println(s"read ${phraseList.length} phrases, ${nameLines.length} theme words")
+  val wordsByLength: Map[Int, Seq[String]] = nameLines.groupBy(_.length)
   val maxWordLength: Int = wordsByLength.keys.max
   val minWordLength: Int = wordsByLength.keys.min
-
-  def solve(): Seq[Solution] = for {
+  val casesToTry: Seq[ThemeContext] =  for {
     wordLen <- minWordLength to maxWordLength
     rot <- 0 until 26
-    newDict: Seq[String] = rawDictList.filter(_.length >= wordLen)
+    newDict: Seq[String] = nameLines.filter(_.length >= wordLen)
     column <- 0 until wordLen
     phraseCnt <- phraseList.indices
     currentPhrase = phraseList(phraseCnt)
     ct = ThemeContext(wordLen, rot, column, currentPhrase, newDict)
-  } yield findThemeSet(requireNumberOfNames)(ct)
+  } yield ct
+
+  def solve(): Seq[Solution] = casesToTry.map(ct => findThemeSet(requireNumberOfNames)(ct))
 }
 
